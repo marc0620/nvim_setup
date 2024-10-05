b49598e1050cf58151ba72b7877bb754031d2001
Marcoooooooooooo <b09901089@g.ntu.edu.tw>
2024-10-05 16:04:10 +0800

    add conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,1 +1,10 @@
-return {}
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatter_by_ft = {
+      c = "clang-format",
+      cpp = "clang-format",
+      python = "ruff_format",
+    },
+  },
+}
